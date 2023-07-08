9c8be4e7bb871fc5826054d7c0f6f0888782023e
maat <matt@mattchan.xyz>
2023-07-08 15:30:54 -0400

    Complete exercise 1.7: add statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,3 @@
-import { logRoles } from "@testing-library/react";
 import { useState } from "react";
 
 const Header = ({ text }) => (
@@ -15,16 +14,62 @@ const App = () => {
   const [neutralNo, setNeutralNo] = useState(0)
   const [badNo, setBadNo] = useState(0)
 
+  const [totalNo, setTotal] = useState(0)
+  const [averageNo, setAverage] = useState(0)
+  const [positiveNo, setPositive] = useState(0)
+  
+  
+  const addGood = () => {
+    const newGood = goodNo + 1
+    const total = badNo + neutralNo + newGood
+    const positive = calcPositive(total, neutralNo + newGood)
+    setPositive(positive)
+    setGoodNo(newGood)
+    setTotal(total)
+    setAverage(calculateAverage(total, badNo, newGood))
+  }
+
+  const addNeutral = () => {
+    const newNeutral = neutralNo + 1
+    const total = badNo + newNeutral + goodNo
+    const positive = calcPositive(total, newNeutral + goodNo)
+    setNeutralNo(newNeutral)
+    setPositive(positive)
+    setTotal(total)
+    setAverage(calculateAverage(total, badNo, goodNo))
+  }
+
+  const addBad = () => {
+    const newBad = badNo + 1
+    const total = newBad + neutralNo + goodNo
+    const positive = calcPositive(total, neutralNo + goodNo)
+    setBadNo(newBad)
+    setPositive(positive)
+    setTotal(total)
+    setAverage(calculateAverage(total, newBad, goodNo))
+  }
+  
+  const calculateAverage = (total, bad, good) => (
+    ((bad * -1) + (good * 1)) / total
+  )
+
+  const calcPositive = (total, good) => (
+    (good / total) * 100
+  )
+  
   return (
     <div className="App">
       <Header text="Give feedback" />
-      <Button handleClick={() => setGoodNo(goodNo + 1)} text="Good" />
-      <Button handleClick={() => setNeutralNo(neutralNo + 1)} text="Neutral" />
-      <Button handleClick={() => setBadNo(badNo + 1)} text="Bad" />
+      <Button handleClick={addGood} text="Good" />
+      <Button handleClick={addNeutral} text="Neutral" />
+      <Button handleClick={addBad} text="Bad" />
       <Header text="Statistics" />
       <p>Good: {goodNo}</p>
       <p>Neutral: {neutralNo}</p>
       <p>Bad: {badNo}</p>
+      <p>Total: {totalNo}</p>
+      <p>Positive: {positiveNo} %</p>
+      <p>Average: {averageNo}</p>
     </div>
   );
 }
