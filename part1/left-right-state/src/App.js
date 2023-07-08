4c8c4945cfb458081fad66f634170d3567b85a5a
maat <matt@mattchan.xyz>
2023-07-08 00:31:43 -0400

    Finish state example

--- part1/left-right-state/src/App.js
+++ part1/left-right-state/src/App.js
@@ -1,8 +1,54 @@
-function App() {
+import { useState } from "react"
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div className="">
+        The app is used by pressing the buttons.
+      </div>
+    )
+  }
+  
   return (
-    <div className="App">
+    <div className="">
+      Button press history: {props.allClicks.join(' ')}
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}> {text} </button>
+)
+
+const App = () => {
+  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  
+  const handleRightClick =() => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
+  const handleLeftClick =() => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text={left} />
+      <Button handleClick={handleRightClick} text={right} />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+export default App
