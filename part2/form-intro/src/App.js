f36c1f70c35e544077bbbdca2e6d4112e8c93628
maat <matt@mattchan.xyz>
2023-07-09 17:34:34 -0400

    Complete form starter with filtering functionality

--- part2/form-intro/src/App.js
+++ part2/form-intro/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Note from './components/Note'
 
-function App() {
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+    console.log('Button clicked', event.target);
+    setNotes(notes.concat(noteObject))
+    setNewNote('') // Clears the field.
+  }
+  
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+          )}
+      </ul>
+
+    <form onSubmit={addNote}>
+          <input 
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+    </form>
     </div>
-  );
+  )
 }
 
 export default App;
