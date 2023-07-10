b2d6194b00d4c59ff5a79acd9f5c6ddd0f470b90
maat <matt@mattchan.xyz>
2023-07-09 21:13:37 -0400

    2.6 — basic phonebook(really a name list)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,32 +1,52 @@
 import { useState } from 'react'
 
-const Numbers = ({ name, numbers }) => {
+const PeopleList = ({ people }) => {
   return (
     <div className=" ">
-      
+      {people.map(person => <p key={person.id}>{person.name}</p>)}
     </div>
   )
 }
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+  const [persons, setPersons] = useState([ ])
   const [newName, setNewName] = useState('')
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const addNewName = (event) => {
+    event.preventDefault()
+    console.log('newName:', newName);
+    const newPerson = {
+      id: newName,
+      name: newName,
+      number: String(5183162704)
+    }
+    setPersons([...persons, newPerson])
+    setNewName('')
+  }
 
   return (
     <div className="App">
-      <form>
+      <form onSubmit={addNewName}>
         <div className="">
-          name: <input />
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div className="">
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-    <div className="debug">debug: {newName} </div>
+      <div className="debug">debug: {newName} </div>
+      <PeopleList people={persons} />
+
     </div>
   );
 }
