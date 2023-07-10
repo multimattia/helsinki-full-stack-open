2a0ce692b72102ad78fc331fa9c918f7f4104585
maat <matt@mattchan.xyz>
2023-07-10 12:19:58 -0400

    Initial useEffect implementation

--- part2/collections-intro/src/App.js
+++ part2/collections-intro/src/App.js
@@ -1,14 +1,32 @@
+import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
 
-const App = ({ notes }) => {
-  return (
-    <div className="App">
-      <h1>Notes</h1>
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+  return(
+    <div>
+      <h2>Notes</h2>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {notes.map(note => <Note key={note.content} note={note} />)}
       </ul>
     </div>
-  );
+  )
+  
+  
 }
 
 export default App;
