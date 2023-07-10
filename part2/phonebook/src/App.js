ec11adcbd0fdeb93597f5e4c9b25a791a8a21610
maat <matt@mattchan.xyz>
2023-07-09 22:26:33 -0400

    2.10 — Refactor form into component, keep state in App

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,12 +8,14 @@ const PeopleList = ({ people }) => {
   )
 }
 
-
-const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newPhoneNumber, setNewPhoneNumber] = useState('')
-
+const PhoneForm = ({
+  persons,
+  setPersons,
+  newName,
+  setNewName,
+  newPhoneNumber,
+  setNewPhoneNumber
+}) => {
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -36,26 +38,47 @@ const App = () => {
       alert(`${newPerson.name} has already been added to the phonebook.`)
     }
   }
+  return (
+    <form onSubmit={addNewPerson}>
+      <div className="">
+        name:
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div className="">
+        phoneNumber:
+        <input
+          value={newPhoneNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div className="">
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
 
   return (
     <div className="App">
-      <form onSubmit={addNewPerson}>
-        <div className="">
-          name: <input value={newName} onChange={handleNameChange}
-          /></div>
-        <div className="">
-          phoneNumber: <input value={newPhoneNumber} onChange={handleNumberChange}
-          />
-        </div>
-        <div className="">
-          <button type="submit">add</button>
-        </div>
-      </form>
       <h2>Numbers</h2>
-      <div className="debug">name: {newName} </div>
-      <div className="debug">number: {newPhoneNumber} </div>
+      <PhoneForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newPhoneNumber={newPhoneNumber}
+        setNewPhoneNumber={setNewPhoneNumber}
+      />
       <PeopleList people={persons} />
-
     </div>
   );
 }
