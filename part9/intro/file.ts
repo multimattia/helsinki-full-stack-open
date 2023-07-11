e54b839f756958a7fd61c189952c3dbd8e743cb0
maat <matt@mattchan.xyz>
2023-07-11 09:51:41 -0400

    TypeScript strict config shenanigans

--- part9/intro/file.ts
+++ part9/intro/file.ts
@@ -1,5 +1,29 @@
-const multiplicator = (a, b, printText) => {
+const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText, a * b);
 }
 
-multiplicator(2, 4, 'Multiplied two by four, the result is:')
+type Operation = 'times' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation): number => {
+    switch (op) {
+        case 'times':
+            return a * b;
+        case 'divide':
+            if (b === 0) throw new Error('Can\'t divide by zero!');
+            return a / b;
+        case 'add':
+            return a + b;
+        default:
+            throw new Error('Operation is not multiply, add or divide!');
+    }
+}
+
+try {
+    console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong: ';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage)
+}
